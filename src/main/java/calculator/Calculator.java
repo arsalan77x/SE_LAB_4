bea553c1420d72bea43b86db7dd8d0d956eb98cf
faraz1380 <Faraz.ghahremani@yahoo.com>
2024-04-25 17:22:53 +0330

    complete features

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,4 +5,33 @@ public class Calculator {
         return a + b;
     }
 
+    public int multiple(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) throws Exception {
+        if (b == 0) {
+            throw new Exception();
+        }
+        return  a / b;
+    }
+
+    public int power(int a, int b) {
+        int result = 1;
+        if (a == 0)
+            return result;
+        else {
+            for (int i = 0; i < b; i++) {
+                result = multiple(result, a);
+            }
+        }
+        if (b < 0) {
+            try{
+                return divide(1, result);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+    }
 }
